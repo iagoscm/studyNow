e16efa9e2d22a2cb6df43a35e002eba7b6cbaa37
Iago Campelo <iagomatosousa@gmail.com>
2022-08-21 01:55:46 -0300

    secret key

--- simplemooc/simplemooc/settings.py
+++ simplemooc/simplemooc/settings.py
@@ -19,6 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+# My secret key
+SECRET_KEY = config("SECRET_KEY")
+ 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
